4735e7503dedb4b4f70325069157bf8aeafa8560
nikitamavrodiy <mna1998@mail.ru>
2022-11-27 15:47:56 +0300

    Homework completed(vers 2)

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -1,8 +1,9 @@
-package pro.sky.calculator.constructor;
+package pro.sky.calculator.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.calculator.service.CalculatorService;
 
@@ -15,6 +16,11 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<String> illegalNumberHandler(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
     @GetMapping()
     public String welcome() {
         return this.calculatorService.welcome();
@@ -22,21 +28,21 @@ public class CalculatorController {
 
     @GetMapping("/plus")
     public String plus(int num1, int num2) {
-        return this.calculatorService.plus(num1, num2);
+        return String.format("%d + %d = %d", num1, num2, calculatorService.plus(num1, num2));
     }
 
     @GetMapping("/minus")
     public String minus(int num1, int num2) {
-        return this.calculatorService.minus(num1, num2);
+        return String.format("%d - %d = %d", num1, num2, calculatorService.minus(num1, num2));
     }
 
     @GetMapping("/multiply")
     public String multiply(int num1, int num2) {
-        return this.calculatorService.multiply(num1, num2);
+        return String.format("%d * %d = %d", num1, num2, calculatorService.multiply(num1, num2));
     }
     @GetMapping("/divide")
     public String divide(int num1, int num2) {
-        return this.calculatorService.divide(num1, num2);
+        return String.format("%d * %d = %d", num1, num2, calculatorService.divide(num1, num2));
     }
 
 
